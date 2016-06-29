5cc3507bde7039f1439b2070db1ecebeed411c69
Malathi Poojari <malathi.s.poojari@hpe.com>
2016-06-29 04:35:40 -0700

    Custom validator for the creation of duplicate vlan through REST Tags: fix, usr TG-1472
    Change-Id: I6a6ee55a984b4ec402bd2cbac588d0130b631e05
    Signed-off-by: Malathi Poojari <malathi.s.poojari@hpe.com>

--- opsplugins/vlan.py
+++ opsplugins/vlan.py
@@ -13,3 +13,16 @@ class VlanValidator(BaseValidator):
         if vlan_name == 'DEFAULT_VLAN_1':
             details = "DEFAULT_VLAN_1 cannot be deleted"
             raise ValidationError(error.VERIFICATION_FAILED, details)
+
+    def validate_modification(self, validation_args):
+        vlan_row = validation_args.resource_row
+        vlan_id = get_column_data_from_row(vlan_row, "id")
+        is_new = validation_args.is_new
+        idl = validation_args.idl
+        if is_new:
+            vlan_id_list = []
+            for ovs_rec in idl.tables["VLAN"].rows.itervalues():
+                vlan_id_list.append(ovs_rec.id)
+            if (len(vlan_id_list) != len(set(vlan_id_list))):
+                details = "VLAN id %d already exists" % (vlan_id)
+                raise ValidationError(error.VERIFICATION_FAILED, details)
