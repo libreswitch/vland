51736bc706817d3048c61588a97b084b2c9824b2
Harshvardhan <harshvardhan@hpe.com>
2016-07-01 03:27:11 -0700

    Removing old tag and and trunk columns from idl cache
    Tags: fix, dev
    TG-1175
    
    Change-Id: I05e2cd343d33cb158b23dd4dfd74a595fbd57a74
    Signed-off-by: Harshvardhan <harshvardhan@hpe.com>

--- src/vland_ovsdb_if.c
+++ src/vland_ovsdb_if.c
@@ -883,8 +883,6 @@ vland_ovsdb_init(const char *db_path)
     ovsdb_idl_add_table(idl, &ovsrec_table_port);
     ovsdb_idl_add_column(idl, &ovsrec_port_col_name);
     ovsdb_idl_add_column(idl, &ovsrec_port_col_vlan_mode);
-    ovsdb_idl_add_column(idl, &ovsrec_port_col_tag);
-    ovsdb_idl_add_column(idl, &ovsrec_port_col_trunks);
     ovsdb_idl_add_column(idl, &ovsrec_port_col_vlan_tag);
     ovsdb_idl_add_column(idl, &ovsrec_port_col_vlan_trunks);
 
