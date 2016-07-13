OpenSwitchNOS/openswitch-ops-vland

chg: dev: Porting test cases to modular framework
Change-Id: Ic21559fa8b3ad87d8ad0fee9a23737ff9cfe2710
Signed-off-by: fonsecamau <fonsecamau@hpe.com>

ops-tests/feature/test_ft_vlan_id_validation.py
@@ -1,0 +1,106 @@
+# -*- coding: utf-8 -*-
+# (C) Copyright 2015 Hewlett Packard Enterprise Development LP
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+##########################################################################
+# Description:       Verify that a VID out of the 802.1Q range or reserved VID
+#                    cannot be set and
+#                    also verifies that a VID which already exists cannot not
+#                    be set again.
+#
+# Author:            Jose Pablo Araya
+#
+# Ported by:         Mauricio Fonseca
+#
+# Topology:          |Switch|
+#
+# Success Criteria:  PASS -> Vlan out of range and repeated not configured
+#
+#                    FAILED -> Vlan out of range or repeated were configured
+#
+##########################################################################
+
+"""
+OpenSwitch Test for vlan related configurations.
+"""
+
+from pytest import mark
+
+TOPOLOGY = """
+# +-------+
+# |  ops1 |
+# +-------+
+
+# Nodes
+[type=openswitch name="OpenSwitch 1"] ops1
+
+# FIXME remove this host when bug is fixed
+[type=host name="Host 1"] hs1
+
+# Links
+# FIXME remove this link when bug is fixed
+ops1:7 -- hs1:1
+"""
+
+
+@mark.test_id(15300)
+def test_ft_vlan_id_validation(topology):
+    ops1 = topology.get('ops1')
+
+    assert ops1 is not None
+
+    def getvlan(dut):
+        vlans = dut.libs.vtysh.show_vlan()
+        return vlans
+
+    def findvlan(dut, pvlan):
+        dic = getvlan(dut)
+        count = 0
+        for key in dic:
+            for elem in dic[key]:
+                if elem == "vlan_id" and dic[key][elem] == pvlan:
+                    count += 1
+        return count
+
+    print("Step 1- Invalid vlan not configured")
+    print("Trying to configure an invalid VLAN")
+    ops1('config terminal')
+    result = ops1('vlan 4095')
+    ops1('end')
+    # FIXME when error check is enabled
+    assert result == '% Unknown command.'
+    print("Verify VLAN was not configured")
+    assert findvlan(ops1, "4095") == 0
+
+    print("Step 2- Verify no previous vlans are configured")
+    countv = getvlan(ops1)
+    if len(countv) == 0:
+        print("No Vlans are configured")
+    else:
+        assert len(countv) > 0
+
+    print("Step 3- Add vlan")
+    with ops1.libs.vtysh.ConfigVlan('2') as ctx:
+        ctx.no_shutdown()
+
+    print("Step 4- Verify vlan was configured")
+    assert findvlan(ops1, "2") == 1
+
+    print("Step 5- Try to add repeated vlan")
+    with ops1.libs.vtysh.ConfigVlan('2') as ctx:
+        ctx.no_shutdown()
+
+    print("Step 6- Validate vlan repeated not configured")
+    assert findvlan(ops1, "2") == 1

ops-tests/feature/test_ft_vlan_state_reason_transition.py
@@ -1,0 +1,160 @@
+# -*- coding: utf-8 -*-
+# (C) Copyright 2015 Hewlett Packard Enterprise Development LP
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+##########################################################################
+# Description: With several vlans configured, bring one vlan up and
+#              confirm its state, witch one port assigned to it verify
+#              its Reason to ok, then issue the command to set the vlan
+#              down and confirm its state. Only one vlan should see a
+#              change in its state.
+#
+# Author:      Jose Pablo Araya
+#
+# Ported by:   Mauricio Fonseca
+#
+# Topology:       |Switch|
+#
+# Success Criteria:  PASS -> Just one of the vlans get the configuration
+#                            perfomed
+#
+#                    FAILED -> If more than one vlan was configured with
+#                              same options
+#
+##########################################################################
+
+"""
+OpenSwitch Test for vlan related configurations.
+"""
+
+from pytest import mark
+
+
+TOPOLOGY = """
+# +-------+                    +-------+
+# |       |     +--------+     |       |
+# |  hs1  <----->  ops1  <----->  hs2  |
+# |       |     +--------+     |       |
+# +-------+                    +-------+
+
+# Nodes
+[type=openswitch name="OpenSwitch 1"] ops1
+[type=host name="Host 1"] hs1
+[type=host name="Host 2"] hs2
+
+# Links
+hs1:1 -- ops1:7
+ops1:8 -- hs2:1
+"""
+
+
+@mark.test_id(15303)
+def test_ft_vlan_state_reason_transition(topology):
+    ops1 = topology.get('ops1')
+    hs1 = topology.get('hs1')
+    hs2 = topology.get('hs2')
+
+    assert ops1 is not None
+    assert hs1 is not None
+    assert hs2 is not None
+
+    def findvlan(dut, pvlan):
+        dic = dut.libs.vtysh.show_vlan()
+        found = False
+        for key in dic:
+            if dic[key]['vlan_id'] == pvlan:
+                    found = True
+        return found
+
+    def vlanstatus(dut, pvlan, elem, status):
+        dic = dut.libs.vtysh.show_vlan()
+        found = False
+        for key in dic:
+            if dic[key]['vlan_id'] == pvlan:
+                if dic[key][elem].lower() == status:
+                    found = True
+        return found
+
+    print("Step 1- Configure workstations")
+    hs1.libs.ip.interface('1', addr='192.168.30.10/24', up=True)
+    hs2.libs.ip.interface('1', addr='192.168.30.11/24', up=True)
+
+    print("Step 2- Adding vlans")
+    with ops1.libs.vtysh.ConfigVlan('2') as ctx:
+        pass
+    with ops1.libs.vtysh.ConfigVlan('3') as ctx:
+        pass
+    with ops1.libs.vtysh.ConfigVlan('4') as ctx:
+        pass
+
+    print("Step 3- Verify vlans were configured")
+    assert findvlan(ops1, "2")
+    assert findvlan(ops1, "3")
+    assert findvlan(ops1, "4")
+
+    print("Step 5- Verify vlan reason before enabling it")
+    assert vlanstatus(ops1, '2', 'reason', 'admin_down')
+    assert vlanstatus(ops1, '3', 'reason', 'admin_down')
+    assert vlanstatus(ops1, '4', 'reason', 'admin_down')
+
+    print("Step 6- Enable one of the configured vlans")
+    with ops1.libs.vtysh.ConfigVlan('2') as ctx:
+        ctx.no_shutdown()
+
+    print("Step 7- Verify vlan status")
+    assert vlanstatus(ops1, '2', 'status', 'down')
+    assert vlanstatus(ops1, '3', 'status', 'down')
+    assert vlanstatus(ops1, '4', 'status', 'down')
+
+    print("Step 8- Verify vlan reason")
+    assert vlanstatus(ops1, '2', 'reason', 'no_member_port')
+    assert vlanstatus(ops1, '3', 'reason', 'admin_down')
+    assert vlanstatus(ops1, '4', 'reason', 'admin_down')
+
+    print("Step 9- Add port to vlan")
+    with ops1.libs.vtysh.ConfigInterface('7') as ctx:
+        ctx.no_routing()
+        ctx.vlan_access(2)
+    with ops1.libs.vtysh.ConfigInterface('8') as ctx:
+        ctx.no_routing()
+        ctx.vlan_access(2)
+
+    print("Step 10- Verify vlan status")
+    assert vlanstatus(ops1, '2', 'status', 'up')
+    assert vlanstatus(ops1, '3', 'status', 'down')
+    assert vlanstatus(ops1, '4', 'status', 'down')
+
+    print("Step 11- Verify vlan reason")
+    assert vlanstatus(ops1, '2', 'reason', 'ok')
+    assert vlanstatus(ops1, '3', 'reason', 'admin_down')
+    assert vlanstatus(ops1, '4', 'reason', 'admin_down')
+
+    print("Step 12- Disable vlan")
+    with ops1.libs.vtysh.ConfigVlan('2') as ctx:
+        ctx.shutdown()
+
+    print("Step 13- Verify vlan status down")
+    assert vlanstatus(ops1, '2', 'status', 'down')
+    assert vlanstatus(ops1, '3', 'status', 'down')
+    assert vlanstatus(ops1, '4', 'status', 'down')
+
+    print("Step 14- Verify vlan reason")
+    assert vlanstatus(ops1, '2', 'reason', 'admin_down')
+    assert vlanstatus(ops1, '3', 'reason', 'admin_down')
+    assert vlanstatus(ops1, '4', 'reason', 'admin_down')
+
+    print("Step 15- Verify ping between hosts")
+    ping = hs1.libs.ping.ping(3, '192.168.30.11')
+    assert ping['transmitted'] == ping['errors'] == 3

ops-tests/feature/test_ft_vlan_state_transition.py
@@ -1,0 +1,112 @@
+# -*- coding: utf-8 -*-
+# (C) Copyright 2015 Hewlett Packard Enterprise Development LP
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+##########################################################################
+# Description: Verify the status of the Vlan change from up to down correctly.
+#               The status of a vlan is changed from down to up when a port is
+#               added to the vlan and the vlan has been brought up with the
+#               respective command.
+#
+# Author:      Jose Pablo Araya
+#
+# Topology:       |Switch|
+#
+# Success Criteria:  PASS -> Vlans status is correctly verifyed
+#                            in every scenario
+#
+#                    FAILED -> If vlan status is wrong showed in one
+#                              of the scenarios
+#
+##########################################################################
+
+"""
+OpenSwitch Test for vlan related configurations.
+"""
+
+
+TOPOLOGY = """
+# +-------+                    +-------+
+# |       |     +--------+     |       |
+# |  hs1  <----->  ops1  <----->  hs2  |
+# |       |     +--------+     |       |
+# +-------+                    +-------+
+
+# Nodes
+[type=openswitch name="OpenSwitch 1"] ops1
+[type=host name="Host 1"] hs1
+
+# Links
+hs1:1 -- ops1:7
+"""
+
+
+def test_ft_vlan_state_transition(topology):  # noqa
+    ops1 = topology.get('ops1')
+
+    assert ops1 is not None
+
+    def findvlan(dut, pvlan):
+        dic = dut.libs.vtysh.show_vlan()
+        found = False
+        for key in dic:
+            if dic[key]['vlan_id'] == pvlan:
+                    found = True
+        return found
+
+    def vlanstatus(dut, pvlan, elem, status):
+        dic = dut.libs.vtysh.show_vlan()
+        found = False
+        for key in dic:
+            if dic[key]['vlan_id'] == pvlan:
+                if dic[key][elem].lower() == status:
+                    found = True
+        return found
+
+    def findport_vlan(dut, pvlan, port):
+        portsearch = dut.ports[port]
+        vlans = dut.libs.vtysh.show_vlan()
+        count = False
+        for key in vlans:
+            if vlans[key]['vlan_id'] == pvlan:
+                for elem in vlans[key]['ports']:
+                    if elem == portsearch:
+                        count = True
+        return count
+
+    print("Step 1- Adding vlans")
+    with ops1.libs.vtysh.ConfigVlan('2') as ctx:
+        pass
+
+    print("Step 2- Verify vlans were configured")
+    assert findvlan(ops1, "2")
+
+    print("Step 3- Verify vlan state")
+    assert vlanstatus(ops1, '2', 'status', 'down')
+
+    print("Step 4- Add port to vlan")
+    with ops1.libs.vtysh.ConfigInterface('7') as ctx:
+        ctx.no_routing()
+        ctx.vlan_access(2)
+
+    print("Step 5- Verify ports assign correctly")
+    assert findport_vlan(ops1, '2', '7')
+
+    print("Step 6- Enable vlan")
+    with ops1.libs.vtysh.ConfigVlan('2') as ctx:
+        ctx.no_shutdown()
+
+    print("Step 7- Verify vlan state")
+    assert vlanstatus(ops1, '2', 'status', 'up')

libreswitch/vland

ops-tests/feature/test_ft_vlan_id_validation.py
@@ -1,0 +1,106 @@
+# -*- coding: utf-8 -*-
+# (C) Copyright 2016 Hewlett Packard Enterprise Development LP
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+##########################################################################
+# Description:       Verify that a VID out of the 802.1Q range or reserved VID
+#                    cannot be set and
+#                    also verifies that a VID which already exists cannot not
+#                    be set again.
+#
+# Author:            Jose Pablo Araya
+#
+# Ported by:         Mauricio Fonseca
+#
+# Topology:          |Switch|
+#
+# Success Criteria:  PASS -> Vlan out of range and repeated not configured
+#
+#                    FAILED -> Vlan out of range or repeated were configured
+#
+##########################################################################
+
+"""
+OpenSwitch Test for vlan related configurations.
+"""
+
+from pytest import mark
+
+TOPOLOGY = """
+# +-------+
+# |  ops1 |
+# +-------+
+
+# Nodes
+[type=openswitch name="OpenSwitch 1"] ops1
+
+# FIXME remove this host when bug is fixed
+[type=host name="Host 1"] hs1
+
+# Links
+# FIXME remove this link when bug is fixed
+ops1:7 -- hs1:1
+"""
+
+
+@mark.gate
+def test_ft_vlan_id_validation(topology, step):
+    ops1 = topology.get('ops1')
+
+    assert ops1 is not None
+
+    def getvlan(dut):
+        vlans = dut.libs.vtysh.show_vlan()
+        return vlans
+
+    def findvlan(dut, pvlan):
+        dic = getvlan(dut)
+        count = 0
+        for key in dic:
+            for elem in dic[key]:
+                if elem == "vlan_id" and dic[key][elem] == pvlan:
+                    count += 1
+        return count
+
+    step("Step 1- Invalid vlan not configured")
+    print("Trying to configure an invalid VLAN")
+    ops1('config terminal')
+    result = ops1('vlan 4095')
+    ops1('end')
+    # FIXME when error check is enabled
+    assert result == '% Unknown command.'
+    print("Verify VLAN was not configured")
+    assert findvlan(ops1, "4095") == 0
+
+    step("Step 2- Verify no previous vlans are configured")
+    countv = getvlan(ops1)
+    if len(countv) == 0:
+        print("No Vlans are configured")
+    else:
+        assert len(countv) > 0
+
+    step("Step 3- Add vlan")
+    with ops1.libs.vtysh.ConfigVlan('2') as ctx:
+        ctx.no_shutdown()
+
+    step("Step 4- Verify vlan was configured")
+    assert findvlan(ops1, "2") == 1
+
+    step("Step 5- Try to add repeated vlan")
+    with ops1.libs.vtysh.ConfigVlan('2') as ctx:
+        ctx.no_shutdown()
+
+    step("Step 6- Validate vlan repeated not configured")
+    assert findvlan(ops1, "2") == 1

ops-tests/feature/test_ft_vlan_state_reason_transition.py
@@ -1,0 +1,164 @@
+# -*- coding: utf-8 -*-
+# (C) Copyright 2016 Hewlett Packard Enterprise Development LP
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+##########################################################################
+# Description: With several vlans configured, bring one vlan up and
+#              confirm its state, witch one port assigned to it verify
+#              its Reason to ok, then issue the command to set the vlan
+#              down and confirm its state. Only one vlan should see a
+#              change in its state.
+#
+# Author:      Jose Pablo Araya
+#
+# Ported by:   Mauricio Fonseca
+#
+# Topology:       |Switch|
+#
+# Success Criteria:  PASS -> Just one of the vlans get the configuration
+#                            perfomed
+#
+#                    FAILED -> If more than one vlan was configured with
+#                              same options
+#
+##########################################################################
+
+"""
+OpenSwitch Test for vlan related configurations.
+"""
+
+from pytest import mark
+from time import sleep
+
+
+TOPOLOGY = """
+# +-------+                    +-------+
+# |       |     +--------+     |       |
+# |  hs1  <----->  ops1  <----->  hs2  |
+# |       |     +--------+     |       |
+# +-------+                    +-------+
+
+# Nodes
+[type=openswitch name="OpenSwitch 1"] ops1
+[type=host name="Host 1"] hs1
+[type=host name="Host 2"] hs2
+
+# Links
+hs1:1 -- ops1:7
+ops1:8 -- hs2:1
+"""
+
+
+@mark.gate
+def test_ft_vlan_state_reason_transition(topology, step):
+    ops1 = topology.get('ops1')
+    hs1 = topology.get('hs1')
+    hs2 = topology.get('hs2')
+
+    assert ops1 is not None
+    assert hs1 is not None
+    assert hs2 is not None
+
+    def findvlan(dut, pvlan):
+        dic = dut.libs.vtysh.show_vlan()
+        found = False
+        for key in dic:
+            if dic[key]['vlan_id'] == pvlan:
+                    found = True
+        return found
+
+    def vlanstatus(dut, pvlan, elem, status):
+        dic = dut.libs.vtysh.show_vlan()
+        found = False
+        for key in dic:
+            if dic[key]['vlan_id'] == pvlan:
+                if dic[key][elem].lower() == status:
+                    found = True
+        return found
+
+    step("Step 1- Configure workstations")
+    hs1.libs.ip.interface('1', addr='192.168.30.10/24', up=True)
+    hs2.libs.ip.interface('1', addr='192.168.30.11/24', up=True)
+
+    step("Step 2- Adding vlans")
+    with ops1.libs.vtysh.ConfigVlan('2') as ctx:
+        pass
+    with ops1.libs.vtysh.ConfigVlan('3') as ctx:
+        pass
+    with ops1.libs.vtysh.ConfigVlan('4') as ctx:
+        pass
+
+    step("Step 3- Verify vlans were configured")
+    assert findvlan(ops1, "2")
+    assert findvlan(ops1, "3")
+    assert findvlan(ops1, "4")
+
+    step("Step 5- Verify vlan reason before enabling it")
+    assert vlanstatus(ops1, '2', 'reason', 'admin_down')
+    assert vlanstatus(ops1, '3', 'reason', 'admin_down')
+    assert vlanstatus(ops1, '4', 'reason', 'admin_down')
+
+    step("Step 6- Enable one of the configured vlans")
+    with ops1.libs.vtysh.ConfigVlan('2') as ctx:
+        ctx.no_shutdown()
+
+    step("Step 7- Verify vlan status")
+    assert vlanstatus(ops1, '2', 'status', 'down')
+    assert vlanstatus(ops1, '3', 'status', 'down')
+    assert vlanstatus(ops1, '4', 'status', 'down')
+
+    step("Step 8- Verify vlan reason")
+    assert vlanstatus(ops1, '2', 'reason', 'no_member_port')
+    assert vlanstatus(ops1, '3', 'reason', 'admin_down')
+    assert vlanstatus(ops1, '4', 'reason', 'admin_down')
+
+    step("Step 9- Add port to vlan")
+    with ops1.libs.vtysh.ConfigInterface('7') as ctx:
+        ctx.no_routing()
+        ctx.vlan_access(2)
+        ctx.no_shutdown()
+    with ops1.libs.vtysh.ConfigInterface('8') as ctx:
+        ctx.no_routing()
+        ctx.vlan_access(2)
+        ctx.no_shutdown()
+
+    step("Step 10- Verify vlan status")
+    assert vlanstatus(ops1, '2', 'status', 'up')
+    assert vlanstatus(ops1, '3', 'status', 'down')
+    assert vlanstatus(ops1, '4', 'status', 'down')
+
+    step("Step 11- Verify vlan reason")
+    assert vlanstatus(ops1, '2', 'reason', 'ok')
+    assert vlanstatus(ops1, '3', 'reason', 'admin_down')
+    assert vlanstatus(ops1, '4', 'reason', 'admin_down')
+
+    step("Step 12- Disable vlan")
+    with ops1.libs.vtysh.ConfigVlan('2') as ctx:
+        ctx.shutdown()
+
+    step("Step 13- Verify vlan status down")
+    assert vlanstatus(ops1, '2', 'status', 'down')
+    assert vlanstatus(ops1, '3', 'status', 'down')
+    assert vlanstatus(ops1, '4', 'status', 'down')
+
+    step("Step 14- Verify vlan reason")
+    assert vlanstatus(ops1, '2', 'reason', 'admin_down')
+    assert vlanstatus(ops1, '3', 'reason', 'admin_down')
+    assert vlanstatus(ops1, '4', 'reason', 'admin_down')
+
+    step("Step 15- Verify ping between hosts")
+    sleep(5)
+    ping = hs1.libs.ping.ping(10, '192.168.30.11')
+    assert ping['transmitted'] == ping['errors'] == 10

ops-tests/feature/test_ft_vlan_state_transition.py
@@ -1,0 +1,112 @@
+# -*- coding: utf-8 -*-
+# (C) Copyright 2016 Hewlett Packard Enterprise Development LP
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+##########################################################################
+# Description: Verify the status of the Vlan change from up to down correctly.
+#               The status of a vlan is changed from down to up when a port is
+#               added to the vlan and the vlan has been brought up with the
+#               respective command.
+#
+# Author:      Jose Pablo Araya
+#
+# Topology:       |Switch|
+#
+# Success Criteria:  PASS -> Vlans status is correctly verifyed
+#                            in every scenario
+#
+#                    FAILED -> If vlan status is wrong showed in one
+#                              of the scenarios
+#
+##########################################################################
+
+"""
+OpenSwitch Test for vlan related configurations.
+"""
+
+
+TOPOLOGY = """
+# +-------+                    +-------+
+# |       |     +--------+     |       |
+# |  hs1  <----->  ops1  <----->  hs2  |
+# |       |     +--------+     |       |
+# +-------+                    +-------+
+
+# Nodes
+[type=openswitch name="OpenSwitch 1"] ops1
+[type=host name="Host 1"] hs1
+
+# Links
+hs1:1 -- ops1:7
+"""
+
+
+def test_ft_vlan_state_transition(topology, step):  # noqa
+    ops1 = topology.get('ops1')
+
+    assert ops1 is not None
+
+    def findvlan(dut, pvlan):
+        dic = dut.libs.vtysh.show_vlan()
+        found = False
+        for key in dic:
+            if dic[key]['vlan_id'] == pvlan:
+                    found = True
+        return found
+
+    def vlanstatus(dut, pvlan, elem, status):
+        dic = dut.libs.vtysh.show_vlan()
+        found = False
+        for key in dic:
+            if dic[key]['vlan_id'] == pvlan:
+                if dic[key][elem].lower() == status:
+                    found = True
+        return found
+
+    def findport_vlan(dut, pvlan, port):
+        portsearch = dut.ports[port]
+        vlans = dut.libs.vtysh.show_vlan()
+        count = False
+        for key in vlans:
+            if vlans[key]['vlan_id'] == pvlan:
+                for elem in vlans[key]['ports']:
+                    if elem == portsearch:
+                        count = True
+        return count
+
+    step("Step 1- Adding vlans")
+    with ops1.libs.vtysh.ConfigVlan('2') as ctx:
+        pass
+
+    step("Step 2- Verify vlans were configured")
+    assert findvlan(ops1, "2")
+
+    step("Step 3- Verify vlan state")
+    assert vlanstatus(ops1, '2', 'status', 'down')
+
+    step("Step 4- Add port to vlan")
+    with ops1.libs.vtysh.ConfigInterface('7') as ctx:
+        ctx.no_routing()
+        ctx.vlan_access(2)
+
+    step("Step 5- Verify ports assign correctly")
+    assert findport_vlan(ops1, '2', '7')
+
+    step("Step 6- Enable vlan")
+    with ops1.libs.vtysh.ConfigVlan('2') as ctx:
+        ctx.no_shutdown()
+
+    step("Step 7- Verify vlan state")
+    assert vlanstatus(ops1, '2', 'status', 'up')